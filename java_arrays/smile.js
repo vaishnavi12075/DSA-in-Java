917d6b131d310a47c52bad2b0a7b9496d9ee02cc
vaishnavi12075 <vaishnavikota1257@gmail.com>
2026-02-11 14:25:40 +0530

    Delete file

--- java_arrays/smile.js
+++
@@ -1,5 +0,0 @@
-const form=document.getElementById("registration-form");
-const username=document.getElementById("username");
-const password=document.getElementById("password");
-const email=document.getElementById("email");
-const confirmpassword=document.getElementById("confirmpassword");
