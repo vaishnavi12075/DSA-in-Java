d9fcf48b0cdaa93ab311e68c735b4e695e0613ee
vaishnavi12075 <vaishnavikota1257@gmail.com>
2026-02-22 18:05:50 +0530

    initial commit

---
+++ java_arrays/hasharray.java
@@ -0,0 +1,27 @@
+public class hasharray {
+    public static int hashtable(int nums[],int val){
+        int maxi=0;
+        for(int i=0;i<nums.length;i++){
+            
+            maxi=Math.max(maxi,nums[i]);
+
+        }
+        
+        int hash[]=new int[maxi+1];
+        for(int i=0;i<nums.length;i++){
+            hash[nums[i]]++;
+        }
+        return hash[val];
+
+    }
+    public static void main(String args[]){
+        int nums[]={1,1,2,3,3,2,3,2,3,3,3};
+        int k=hashtable(nums,3);
+        System.out.println("frequency of 3 is: "+k);
+
+
+
+
+    }
+    
+}
